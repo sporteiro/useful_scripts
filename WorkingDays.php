4b62ad57f07accb211f32a129cbe8faa86795074
sporteiro <45196058+sporteiro@users.noreply.github.com>
2022-02-24 08:46:58 +0000

    correct indentation
    correct indentation

--- WorkingDays.php
+++ WorkingDays.php
@@ -6,35 +6,35 @@ class WorkingDays {
 
 	public function main($start)  {
 
-        //set the period we want to obtain working days from
-        //$start = "2022-01-01";
-        $begin = new DateTime($start);
-        $end = new DateTime($start);
-        $end = $end->modify( '+365 days' );
-        
-        //create an array for notworkingdays, it can be populate with bank holidays or any other non working day
-        $notworkingdays_array = array();
-        //create an array for working days;
-        $workingdays_array = array();
+		//set the period we want to obtain working days from
+		//$start = "2022-01-01";
+		$begin = new DateTime($start);
+		$end = new DateTime($start);
+		$end = $end->modify( '+365 days' );
 
-        //interval for all days of the first period
-        $interval = new DateInterval('P1D');
-        $daterange = new DatePeriod($begin, $interval ,$end);
+		//create an array for notworkingdays, it can be populate with bank holidays or any other non working day
+		$notworkingdays_array = array();
+		//create an array for working days;
+		$workingdays_array = array();
+
+		//interval for all days of the first period
+		$interval = new DateInterval('P1D');
+		$daterange = new DatePeriod($begin, $interval ,$end);
 
 
-        //populate notworkingdays_array with weekends
-        foreach($daterange as $date) {
-            if ($this->isWeekend($date)==1)	{
-                array_push($notworkingdays_array,$date->format("Y-m-d"));
-            }
-        } 
-        //populate working_days array
-        foreach($daterange as $date) {
-            if (!in_array($date->format("Y-m-d"), $notworkingdays_array)) {
-                array_push($workingdays_array,$date->format("Y-m-d"));
-            }
-        }  
-        return $workingdays_array;
+		//populate notworkingdays_array with weekends
+		foreach($daterange as $date) {
+		    if ($this->isWeekend($date)==1)	{
+			array_push($notworkingdays_array,$date->format("Y-m-d"));
+		    }
+		} 
+		//populate working_days array
+		foreach($daterange as $date) {
+		    if (!in_array($date->format("Y-m-d"), $notworkingdays_array)) {
+			array_push($workingdays_array,$date->format("Y-m-d"));
+		    }
+		}  
+		return $workingdays_array;
     }
     //check if a given date is a weekend
     public function isWeekend($date) {
